959fe752494c7126ee82e2b367ffe9217c8307bf
SuryaVipul <suryashukla977@gmail.com>
2025-10-03 20:13:04 +0530

    setup CineHub with Vite and tailwind css

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  test: {
+    environment: "jsdom", // needed for React component testing
+    globals: true, // allows "expect" and other globals
+  },
+});
